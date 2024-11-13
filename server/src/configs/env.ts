4791abf649dcf6a34779a69f78ce78d51cd27e98
catjofupit3r <roman.barmak@uplab.io>
2024-11-14 01:23:24 +0200

    build: fix start script

--- server/src/configs/env.ts
+++ server/src/configs/env.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+if (NODE_ENV === 'development') {
+    dotenv.config();
+}
 
 export const HOST = process.env.HOST || 'localhost';
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
 export const MONGO_USER = process.env.MONGO_USER || 'user';
 export const MONGO_PASS = process.env.MONGO_PASS || '123123';
